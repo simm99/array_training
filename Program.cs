1f20e91093caae7cbfdb84aa146a928f7da6e525
simm99 <100690242+simm99@users.noreply.github.com>
2025-02-01 21:39:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentList
+{
+    class Students
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students do you want to add to you class: ");
+            string studentString = Console.ReadLine();
+            int amount;
+
+            // Validera att användaren skriver ett giltigt tal
+            while (!int.TryParse(studentString, out amount) || amount <= 0)
+            {
+                Console.Write("Invalid input. Please enter a valid number greater than 0: ");
+                studentString = Console.ReadLine();
+            }
+
+            bool IsValidName(string name)
+            {
+                foreach (char c in name)
+                {
+                    if (!char.IsLetter(c) && c != ' ')
+                    {
+                        return false;
+                    }
+                }
+                return !string.IsNullOrWhiteSpace(name);
+            }
+
+            string[] studentList = new String[amount];
+
+
+            Console.WriteLine("\nPlease type in the names of the students: ");
+
+            for (int i = 0; i < studentList.Length; i++)
+            {
+                string studentName = "";
+                // Tvinga användaren att skriva in ett namn
+                while(!IsValidName(studentName))
+                {
+                    Console.WriteLine($"Enter name for student {i + 1}: ");
+                    studentName = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(studentName))
+                    {
+                        Console.WriteLine("You have to enter a name. Try again: ");
+                    }
+                }
+                studentList[i] = studentName;
+            }
+            
+            Console.WriteLine("\nHere the students are in Alphabetically order: ");
+            Array.Sort(studentList);
+            for (int i = 0; i < studentList.Length; i++)
+            {
+                Console.WriteLine(studentList[i]);
+            }
+        }  
+    }
+}
